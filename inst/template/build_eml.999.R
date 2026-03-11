f47070ab5da195b86a3be07ba5300ce901dc67c9
Gregory Maurer <gregmaurer@gmail.com>
2026-03-11 16:04:39 -0600

    docs: Parameter name changes in template and README

--- inst/template/build_eml.999.R
+++ inst/template/build_eml.999.R
@@ -46,9 +46,9 @@ load_metabase_cred(mbcred_path)
 load_destination_cred(destcred_path)
 
 # Now create or update the dataset on EDI...
-# You must pass `dry.run=FALSE` to really publish the data. Make sure to check
+# You must pass `dry_run=FALSE` to really publish the data. Make sure to check
 # dataset identifiers, revision numbers, eml, and other details first.
-publish_dataset_edi(id, mbname, mbcred, edicred, edi.env=env)
+publish_dataset_edi(id, mbname, mbcred, edicred, edi_env=env)
 
 # Clean up
 remove(mbcred, edicred, mbname, mbcred_path, destcred_path)
